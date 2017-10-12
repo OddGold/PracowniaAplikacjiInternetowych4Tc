68267a4f0ee3c30c82f0fac3a608b233cbcc19f3
oddgold <bartekwiechec@onet.eu>
2017-10-12 11:13:07 +0200

    xd

--- Pracownia Aplikacji Internetowych Klasa 4/jQuery/11.10.2017/jQuery-class.js
+++ Pracownia Aplikacji Internetowych Klasa 4/jQuery/11.10.2017/jQuery-class.js
@@ -1,6 +1,22 @@
 jQuery(document).ready(function(){
 	
+	$('#tlo1').click(function(){
+		$('#akapit').addClass('tlo1');
+	})
 	
+	/*$('#tlo2').click(function(){
+		$('#akapit').addClass('tlo2');
+	})*/
+	
+	$('#tlo2').click(function(){
+		$('#akapit').toggleClass('tlo2');
+	})
+	
+	$('#tlo3').click(function(){
+		//$('#akapit').removeClass('tlo1');
+		//$('#akapit').removeClass();
+		alert($('#akapit').hasClass('tlo1'))
+	})
 	
 	
 	
