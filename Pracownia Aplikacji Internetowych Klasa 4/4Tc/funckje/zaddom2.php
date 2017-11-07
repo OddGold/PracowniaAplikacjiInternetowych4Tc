cb9b83dd7885c03d147ee030d88fc7330b83c442
oddgold <bartekwiechec@onet.eu>
2017-11-07 20:05:59 +0100

    xd

--- Pracownia Aplikacji Internetowych Klasa 4/4Tc/funckje/zaddom2.php
+++ Pracownia Aplikacji Internetowych Klasa 4/4Tc/funckje/zaddom2.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
+if(!isset($_SESSION['spis'])){
 $spisPensji = array(
-    "styczen" => array(),
+    "Styczen" => array(),
     "Luty" => array(),
     "Marzec" => array(),
     "Kwiecień" => array(),
@@ -14,23 +15,39 @@ $spisPensji = array(
     "Listopad" => array(),
     "Grudzien" => array()
 );
+    $_SESSION['miesiac'] = array();
+    $_SESSION['spis'] = $spisPensji;
+}
+$czyJest=false;
 $pensja = $_GET['pensja'];
 $miesiac = $_GET['miesiac'];
+//branie daty
 $data = getdate();
 $rok = $data['year']; 
-$miesiac = $data['mon']; 
+$miesiac2 = $data['mon']; 
+if($miesiac2<10) $miesiac2='0'.$miesiac2;
 $dzien = $data['mday']; 
+if($dzien<10) $dzien='0'.$dzien;
 $godzina = $data['hours'];
+if($godzina<10) $godzina='0'.$godzina;
 $minut = $data['minutes'];
+if($minut<10) $minut='0'.$minut;
 $sekund = $data['seconds'];
-$dataPelna = "$dzien/$miesiac/$rok";
+if($sekund<10) $sekund='0'.$sekund;
+$dataPelna = "$dzien/$miesiac2/$rok";
 $czas = "$godzina:$minut:$sekund";
-if($_SESSION['miesiac']==$miesiac){
-$_SESSION['komunikat'] = '<span style="color: red">BŁĄD. MIESIĄC ZOSTAŁ JUZ PODANY</span>'; 
+
+foreach($_SESSION['miesiac'] as $a){
+    if($a == $miesiac) $czyJest = true;
+}
+if($czyJest){
+    $_SESSION['komunikat'] = '<span style="color: red">BŁĄD. MIESIĄC ZOSTAŁ JUZ PODANY</span>'; 
 }else{
-$spisPensji[$miesiac]['pensja'] = $pensja;
-$_SESSION['komunikat'] = '<span style="color: red">Dodano pomyślnie pensje, czyli pensja w miesiącu '.$miesiac.' wynosi '.$spisPensji[$miesiac]['pensja'].' zł<br>Dodano w dniu '.$dataPelna.' o godzinie: '.$czas.'</span>';
-$_SESSION['miesiac'] = $miesiac;
+    $_SESSION['spis'][$miesiac]['pensja'] = $pensja;
+    $_SESSION['spis'][$miesiac]['data'] = $dataPelna;
+    $_SESSION['spis'][$miesiac]['czas'] = $czas;
+    $_SESSION['komunikat'] = '<span style="color: red">Dodano pomyślnie pensje, czyli pensja w miesiącu '.$miesiac.' wynosi '.$_SESSION['spis'][$miesiac]['pensja'].' zł<br>Dodano w dniu '.$_SESSION['spis'][$miesiac]['data'].' o     godzinie: '.$_SESSION['spis'][$miesiac]['czas'].'</span>';
+    array_push($_SESSION['miesiac'],$miesiac);
 }
 header("Location: zaddom.php");
 
